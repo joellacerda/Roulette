271b6271c3e1f9561ffbe9c36fb23185e703c0bb
trrrrryuttttt <joellacerdaol@gmail.com>
2025-05-07 19:53:52 -0300

    ♻️ refactor(login): Refactor project structure and remove storyboard
    - Moved ViewController and related files to Login folder
    - Moved app delegate and resources to Resource folder
    - Created LoginScreen file
    - Removed storyboard references

---
+++ Roulette/Login/Screen/LoginScreen.swift
@@ -0,0 +1,26 @@
+//
+//  LoginScreen.swift
+//  Roulette
+//
+//  Created by Joel Lacerda on 07/05/25.
+//
+
+import UIKit
+
+// MARK: ViewCode
+// 1) Criar a Screen()
+// 2) Chamar o construtor
+// 3) Chamar no loadView
+// 4) Criar os elemtnos
+
+class LoginScreen: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
